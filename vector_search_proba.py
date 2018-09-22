31f2db1dae84cd9359722e225c388809d6e585b3
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2018-09-22 10:39:00 +0300

    Improve implementation speed

--- vector_search_proba.py
+++ vector_search_proba.py
@@ -2,17 +2,35 @@ from functools import reduce
 import operator
 
 
-def product(iterable):
+def prod(iterable):
     return reduce(operator.mul, iterable, 1)
 
 
 def calculate_probability_to_find_match(item_count, vectors_per_item, nn_pct):
+    """
+    Calculates the probability of finding a match for a given item in a
+    nearest neighbor search when vectors for items are placed randomly in a space.
+
+    :param item_count: The number of items for which vectors are generated
+    :param vectors_per_item: The number of vectors per item
+    :param nn_pct: The percentage of items to search in by nearest neighbor algorithm.
+                   For instance if nn_pct is 0.01 and item_count is 1000 then the
+                   0.1 * 1000 = 10 nearest neighbors are searched. The probability
+                   returned is the probability of finding the given item in the
+                   10 nearest neighbors.
+
+    :return: The probability of finding the vector for a given item in the NN search.
+    """
     vectors = item_count * vectors_per_item
     nn_count = round(item_count * nn_pct)
 
-    prob_no_match = product(range(vectors - nn_count - vectors_per_item + 1,
-                                  vectors - vectors_per_item + 1)) \
-                    / product(range(vectors - nn_count + 1, vectors + 1))
+    numerator_range = range(vectors - nn_count - vectors_per_item + 1,
+                            vectors - vectors_per_item + 1)
+    denominator_range = range(vectors - nn_count + 1, vectors + 1)
+
+    prob_no_match = 1
+    for num_item, dem_item in zip(numerator_range, denominator_range):
+        prob_no_match *= num_item / dem_item
 
     return 1 - prob_no_match
 
