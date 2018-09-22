2c308f81067d0d32669b11bf938438a60c8e78ac
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2018-09-22 10:29:46 +0300

    Implementation works with more than one vector per item

--- test_calculate_probability_to_find_match.py
+++ test_calculate_probability_to_find_match.py
@@ -19,3 +19,31 @@ class TestCalculateProbabilityMatch(TestCase):
         for nn_pct, item_count in product(nn_pcts, item_count):
             result = calculate_probability_to_find_match(item_count, vectors_per_item, nn_pct)
             self.assertCorrectResult(result, nn_pct, item_count, vectors_per_item, nn_pct)
+
+    def test_calc_prob_one_nn(self):
+        item_count = [1000, 2000, 5000, 10000, 20000, 50000, 100000, 500000, 1000000]
+        vectors_per_item = [1, 5, 10, 100]
+        for vectors_per_item, item_count in product(vectors_per_item, item_count):
+            nn_pct = 1 / item_count
+            result = calculate_probability_to_find_match(item_count, vectors_per_item, nn_pct)
+            self.assertCorrectResult(result, nn_pct, item_count, vectors_per_item, nn_pct)
+
+    def test_calc_prob_two_vectors_per_item(self):
+        nn_pct = 0.4
+        item_count = 5
+
+        result_1_item = calculate_probability_to_find_match(item_count, 1, nn_pct)
+        self.assertCorrectResult(result_1_item, nn_pct, item_count, 1, nn_pct)
+
+        result_2_items = calculate_probability_to_find_match(item_count, 2, nn_pct)
+        self.assertCorrectResult(result_2_items, 0.3777777777777, item_count, 2, nn_pct)
+
+    def test_calc_prob_ten_vectors_per_item(self):
+        nn_pct = 0.5
+        item_count = 10
+
+        result_1_item = calculate_probability_to_find_match(item_count, 1, nn_pct)
+        self.assertCorrectResult(result_1_item, nn_pct, item_count, 1, nn_pct)
+
+        result_10_items = calculate_probability_to_find_match(item_count, 10, nn_pct)
+        self.assertCorrectResult(result_10_items, 0.4162476331, item_count, 2, nn_pct)
--- vector_search_proba.py
+++ vector_search_proba.py
@@ -10,7 +10,8 @@ def calculate_probability_to_find_match(item_count, vectors_per_item, nn_pct):
     vectors = item_count * vectors_per_item
     nn_count = round(item_count * nn_pct)
 
-    prob_no_match = product(range(vectors - nn_count, vectors)) \
+    prob_no_match = product(range(vectors - nn_count - vectors_per_item + 1,
+                                  vectors - vectors_per_item + 1)) \
                     / product(range(vectors - nn_count + 1, vectors + 1))
 
     return 1 - prob_no_match
